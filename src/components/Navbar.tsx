cf88a3dc6589de5e9ddff4e99e16093a4260a0e7
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-30 21:04:48 +0530

    Installing and Seting Up Clerk into our Project for Authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { ExternalLinkIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -20,7 +21,14 @@ const Navbar = () => {
             </Link>
           </nav>
         </div>
-        <Button>Sign in</Button>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button variant={"outline"}>Sign in</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
   );
