aa4d6d1349809ff79de0ed2c79d4bd09bd0d06bb
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-30 14:03:18 +0530

    Complete Navbar Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="space-y-2 mt-10">
+      <h1 className="text-3xl font-bold sm:text-5xl">Share Your Data</h1>
+      <p className="max-w-[600px] text-muted-foreground md:text-xl">
+        Our app makes easy to share your{" "}
+        <span className="text-bold bg-yellow-400 text-black">JSON</span> data
+        with others. Simply authenticate and upload your data.
+      </p>
+    </div>
+  );
 }
