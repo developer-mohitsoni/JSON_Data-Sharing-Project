e5cb5cfea9039c0ea901c06d16194d691b83c111
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-05 11:16:55 +0530

    Storing User Data from Clerk into PostgresQL

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,30 @@
 import JsonEditor from "@/components/JSON-Editor";
-import React from "react";
+import { currentUser } from "@clerk/nextjs/server";
+import prisma from "../../../DB/db.config";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+
+  const loggedInUser = await prisma.user.findUnique({
+    where: {
+      clerkUserId: user.id,
+    },
+  });
+
+  if (!loggedInUser) {
+    await prisma.user.create({
+      data: {
+        clerkUserId: user.id,
+        name: `${user.firstName} ${user.lastName}`,
+        imageUrl: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+  }
   return (
     <div>
       <div className="my-8">
